4c01daf914d2150f238a297aeb436e0a11b215eb
Leonardo Castilho <leonardo.castilho@viannasempre.com.br>
2022-09-03 11:14:25 -0300

    Sobrecarga e classe diretor
    Sobrecarga e criação da classe diretor com o mesmo método getBonificação para realizar a sobrecarga.

--- ByteBank_ADM/Funcionarios/Diretor.cs
+++ ByteBank_ADM/Funcionarios/Diretor.cs
@@ -1,13 +1,14 @@
 ﻿namespace ByteBank_ADM.Funcionarios
 {
-    public class Funcionario
+    public class Diretor
     {
         public string? Nome { get; set; }
         public string? Cpf { get; set; }
         public double Salario { get; set; }
+
         public double getBonificacao()
         {
-            return Salario * 0.1;
+            return Salario;
         }
     }
 }
--- ByteBank_ADM/Program.cs
+++ ByteBank_ADM/Program.cs
@@ -8,3 +8,10 @@ funcionario.Cpf = "12345698-12";
 funcionario.Salario = 2000;
 
 Console.WriteLine("Bonificação: " + funcionario.getBonificacao());
+
+Diretor diretor = new Diretor();
+diretor.Nome = "Diretor";
+diretor.Cpf = "53445421-33";
+diretor.Salario = 5000;
+
+Console.WriteLine("Bonificação: " + diretor.getBonificacao());
