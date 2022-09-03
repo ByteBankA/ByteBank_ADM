bf5513a79bd90d10ee6debb66a96bc2adf97d798
Leonardo Castilho <leonardo.castilho@viannasempre.com.br>
2022-09-03 10:16:59 -0300

    Commit Inicial

---
+++ ByteBank_ADM/Funcionarios/Funcionario.cs
@@ -0,0 +1,14 @@
+﻿namespace ByteBank_ADM.Funcionarios
+{
+    public class Funcionario
+    {
+        private string Nome { get; set; }
+        private string Cpf { get; set; }
+        private double Salario { get; set; }
+
+        public double getBonificacao()
+        {
+            return Salario * 0.1;
+        }
+    }
+}
---
+++ ByteBank_ADM/Program.cs
@@ -0,0 +1,10 @@
+﻿using ByteBank_ADM.Funcionarios;
+
+Console.WriteLine("Boas Vindas! Você está no ByteBank Admnistração");
+
+Funcionario funcionario = new Funcionario();
+funcionario.Nome = "João";
+funcionario.Cpf = "12345698-12";
+funcionario.Salario = 2000;
+
+Console.WriteLine("Bonificação: " + funcionario.getBonificacao());
